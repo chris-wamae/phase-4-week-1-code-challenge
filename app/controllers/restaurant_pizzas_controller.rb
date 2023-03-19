d561b72c90f84178d3667c57c6f11d8572110ade
chris-wamae <chriswamae123@gmail.com>
2023-03-19 15:58:41 +0300

    Added controllers and error handling to restaurant_pizzas

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,4 +1,30 @@
 class RestaurantPizzasController < ApplicationController
-    def find_by_id
+    protect_from_forgery
+    rescue_from ActiveRecord::RecordNotFound, with: :error_message
+    #checks if the a pizza with the passed pizza_id exists
+    #checks if a restaurant with the passed restaurant_id exists
+    #if they dont it throws an error
+    def create
+    Pizza.find(params[:pizza_id])
+    Restaurant.find(params[:restaurant_id])
+    new_restaurant_pizza = RestaurantPizza.create(allowed_params)
+    if new_restaurant_pizza.valid?
+    render json: new_restaurant_pizza
+    else
+    render json: {error:"Validation errors"}, status:422
     end
+    end
+
+    
+    private
+
+    def allowed_params
+        params.permit(:price, :restaurant_id, :pizza_id)
+    end
+
+    def error_message
+        render json: {error:"Validation errors"}, status:422
+    end
+
+
 end
