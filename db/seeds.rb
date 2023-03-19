0feb3a2620ace22f8f3f782d3ecb09471ef1e79f
chris-wamae <chriswamae123@gmail.com>
2023-03-19 13:51:16 +0300

    Added table relatioonships and seeded data to test them

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Seeeeding.."
+restaurant = Restaurant.create(name:"UKO",address:"102020-Naiorbi")
+pizza = Pizza.create(name:"Hawiw",ingredients:"A lil bread")  
+restaurant_pizzas1 = RestaurantPizza.create(price:5, restaurant_id:1, pizza_id:1)
+puts "Done seeding!"
